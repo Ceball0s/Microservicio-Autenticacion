ed2a4773208038c95968d89a03b810ee58c9826c
Ceball0s <ceballosmiguel44@gmail.com>
2025-06-21 18:47:08 -0500

    Conexion con eureka

--- src/main/java/com/Auth/AuthApplication.java
+++ src/main/java/com/Auth/AuthApplication.java
@@ -5,7 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 @SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
 public class AuthApplication {
 
     public static void main(String[] args) {
